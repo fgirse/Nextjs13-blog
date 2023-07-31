0007eed271b62290e840557f5d86afb64c7ff134
Frank Girse <fgirse@mac-mini-von-frank.home>
2023-07-31 05:40:43 +0200

    2er commit

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,22 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/Users/fgirse/Nextjs13-blog/app/layout.tsx": [
+      "/Users/fgirse/Nextjs13-blog/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "/Users/fgirse/Nextjs13-blog/app/globals.css"
+    ]
+  },
+  "cssModules": {
+    "/Users/fgirse/Nextjs13-blog/app/page": [
+      "/Users/fgirse/Nextjs13-blog/app/globals.css",
+      "/Users/fgirse/Nextjs13-blog/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ],
+    "/Users/fgirse/Nextjs13-blog/app/register/page": [
+      "/Users/fgirse/Nextjs13-blog/app/globals.css",
+      "/Users/fgirse/Nextjs13-blog/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ],
+    "/Users/fgirse/Nextjs13-blog/app/login/page": [
+      "/Users/fgirse/Nextjs13-blog/app/globals.css",
+      "/Users/fgirse/Nextjs13-blog/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ]
+  }
+}
